04446a8bcdd47e3018fd619c8e0fc50e55bf553e
mosem <mandelmoshe@gmail.com>
2023-01-17 00:13:00 +0200

    filtering in time domain complete. with multiprocessing.

--- data_prep/filter_data.py
+++ data_prep/filter_data.py
@@ -1,17 +1,49 @@
+import sys
+sys.path.append('/cs/labs/adiyoss/moshemandel/random-bwe/aero')
+
 import argparse
 import json
 import os
+from itertools import groupby
+
 import torchaudio
 from pathlib import Path
+from multiprocessing import Process
+from multiprocessing import Pool
+
+from tqdm import tqdm
 
 from data_prep.low_pass_filter import LowPassFilter
 
+def get_out_path(input_path, out_dir):
+    parent_dirs = str(Path(input_path).parent).split(os.sep)[-2:]
+    out_path = os.path.join(out_dir, *parent_dirs, Path(input_path).name)
+    return out_path
+
+def filter_and_save(input_path, alpha, beta, out_dir, cutoff_ratio):
+    low_pass_filter = LowPassFilter(cutoff_ratio)
+    out_path = get_out_path(input_path, out_dir)
+    input, sr = torchaudio.load(input_path)
+    out = low_pass_filter(input, alpha, beta)
+    torchaudio.save(out_path, out, sr)
+    print(f'Done filtering {Path(input_path).name}')
+
+def filter_and_save_dir(dir_files_meta, out_dir, cutoff_ratio):
+    for path, length, alpha, beta in dir_files_meta:
+        filter_and_save(path, alpha, beta, os.path.join(out_dir, Path(path).name), cutoff_ratio)
+
+def create_dirs(files_meta_by_dir, out_dir):
+    for lst in files_meta_by_dir:
+        input_path = Path(lst[0][0]).parent
+        parent_dirs = str(input_path).split(os.sep)[-2:]
+        out_path = os.path.join(out_dir, *parent_dirs)
+        os.makedirs(out_path, exist_ok=True)
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Resample data.')
-    parser.add_argument('--json_dir', type=int, help='path to dir containing json file')
+    parser.add_argument('--json_dir', help='path to dir containing json file')
     parser.add_argument('--out_dir', help='directory to write target files')
-    parser.add_argument('--cutoff', type=int, help='target sample rate')
+    parser.add_argument('--cutoff_ratio', type=int, help='target sample rate')
     return parser.parse_args()
 
 def main():
@@ -21,15 +53,41 @@ def main():
     source_json_path = os.path.join(json_dir, 'hr.json')
     target_json_path = os.path.join(json_dir, 'lr.json')
 
-    low_pass_filter = LowPassFilter(args.cutoff)
-    files_meta = json.load(source_json_path)
 
-    for path, length, alpha, beta in files_meta:
-        name = Path(path).stem
-        out_path = os.path.join(args.out_dir, name)
-        in, sr = torchaudio.load(path)
-        out = low_pass_filter(in, alpha, beta)
-        torchaudio.save(out)
+    with open(source_json_path) as f:
+        files_meta = json.load(f)
+    files_meta_by_dir = [list(v) for i, v in groupby(files_meta, lambda x: Path(x[0]).parent)]
+    create_dirs(files_meta_by_dir, args.out_dir)
+
+    # low_pass_filter = LowPassFilter(args.cutoff_ratio)
+    # for path, length, alpha, beta in tqdm(files_meta):
+    #     name = Path(path).name
+    #     parent_dirs = str(Path(path).parent).split(os.sep)[-2:]
+    #     out_dir = os.path.join(args.out_dir, *parent_dirs)
+    #     out_path = os.path.join(out_dir, name)
+    #     input, sr = torchaudio.load(path)
+    #     out = low_pass_filter(input, alpha, beta)
+    #     torchaudio.save(out_path, out, sr)
+    #     print(f'Done filtering {name}')
+
+    #
+    #
+    print('filtering in parallel...')
+    with Pool() as pool:
+        pool.starmap_async(filter_and_save_dir,
+                                 [(meta, args.out_dir, args.cutoff_ratio) for meta in files_meta_by_dir]).get()
+
+
+    # processes = []
+    # for path, length, alpha, beta in files_meta:
+    #     parent_dirs = str(Path(path).parent).split(os.sep)[-2:]
+    #     out_dir = os.path.join(args.out_dir, *parent_dirs)
+    #     p = Process(target=filter_and_save, args=(path, alpha, beta, os.path.join(out_dir, Path(path).name), args.cutoff_ratio))
+    #     p.start()
+    #     processes.append(p)
+    # for p in processes:
+    #     p.join()
+
 
 
 
--- data_prep/low_pass_filter.py
+++ data_prep/low_pass_filter.py
@@ -13,44 +13,45 @@ def sinc(x: torch.Tensor):
    return torch.where(x == 0, torch.tensor(1., device=x.device, dtype=x.dtype), torch.sin(x) / x)
 
 
-def compute_cutoff_t(cutoff, alpha, beta, t, fs=8000):
+def compute_cutoff_t(cutoff_ratio, alpha, beta, t, fs=8000):
     """
 
     """
-    return (math.pi / cutoff + alpha * math.sin(beta * t)) * fs / 2 * math.pi
+    return (math.pi / cutoff_ratio + alpha * math.sin(beta*t/fs)) / (2 * math.pi)
 
 
-def filter_t(cutoff, alpha, beta, t, fs=8000, filter_len=8):
-    half_size = int(filter_len / 2)
-    cutoff = compute_cutoff_t(cutoff, alpha, beta, t, fs)
+def filter_t(cutoff_ratio, alpha, beta, t, fs=8000, zeros=8):
+    half_size = int(zeros * cutoff_ratio / 2)
+    cutoff_t = compute_cutoff_t(cutoff_ratio, alpha, beta, t, fs)
     window = torch.hann_window(2 * half_size + 1, periodic=False)
     time = torch.arange(-half_size, half_size + 1)
-    filter = 2 * cutoff * window * sinc(2 * cutoff * math.pi * time)
+    filter = 2 * cutoff_t * window * sinc(2 * cutoff_t * math.pi * time)
     # Normalize filter to have sum = 1, otherwise we will have a small leakage
     # of the constant component in the input signal.
     filter /= filter.sum()
 
     return filter
 
-def compute_conv_mat(n, cutoff, alpha, beta, fs=8000, filter_len=8):
-    half_size = int(filter_len / 2)
-    mat = filter_len((n + half_size, n + half_size))
+def compute_conv_mat(n, cutoff_ratio, alpha, beta, fs=8000, zeros=8):
+    half_size = int(zeros * cutoff_ratio / 2)
+    filter_size = half_size * 2 + 1
+    mat = torch.zeros((n, n + filter_size))
     for t in range(mat.shape[0]):
-        mat[t,t:t + filter_len] = torch.flip(filter_t(cutoff, alpha, beta, t, fs, filter_len), [0])
-    return mat[:, half_size:-half_size]
+        mat[t, t:t + filter_size] = torch.flip(filter_t(cutoff_ratio, alpha, beta, t, fs, zeros), [0])
+    return mat[:, half_size:-half_size-1]
 
 
 
 class LowPassFilter(torch.nn.Module):
 
-    def __init__(self, cutoff, fs: float = 8000, filter_len: float = 8):
+    def __init__(self, cutoff_ratio, fs: float = 8000, zeros: float = 8):
         super().__init__()
-        self.cutoff = cutoff
+        self.cutoff_ratio = cutoff_ratio
         self.fs = fs
-        self.filter_len = filter_len
+        self.zeros = zeros
 
     def forward(self, input, alpha, beta):
-        n = len(input)
-        conv_mat = compute_conv_mat(n, self.cutoff, alpha, beta, self.fs, self.filter_len)
-        output = torch.matmul(conv_mat, input)
+        n = input.shape[-1]
+        conv_mat = compute_conv_mat(n, self.cutoff_ratio, alpha, beta, self.fs, self.zeros)
+        output = torch.matmul(conv_mat, input.T).T
         return output

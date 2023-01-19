4712ddf477b38af0f9529a26f5aa2e2384c61fbf
mosem <mandelmoshe@gmail.com>
2023-01-19 19:23:47 +0200

    filtering in time domain complete. and efficient. added also gaussian noise transform.

---
+++ bash/train-condition-one-hot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+. /cs/labs/adiyoss/moshemandel/random-bwe/aero/venv/bin/activate
+
+python train.py \
+  dset=4-8 \
+  experiment=aero_4-8_512_256 \
+  experiment.aero.condition_on_cutoff=true \
+  'experiment.name=aero-condition-one-hot-nfft-512-hl-256' \
+
